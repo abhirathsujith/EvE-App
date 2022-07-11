b594d104b5b94586efa38a5edd3b74a0625e7dc0
AbhirathSP <abhirathsp2@gmail.com>
2022-07-12 04:12:03 +0530

    Initial Commit

---
+++ EvE/EvEApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvEApp.swift
+//  EvE
+//
+//  Created by Abhirath Sujith on 12/07/22.
+//
+
+import SwiftUI
+
+@main
+struct EvEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
